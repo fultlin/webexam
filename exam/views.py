3919659a661cc890a01da3878cf8af7c8c115d24
fultlin <ugolok.kita@gmail.com>
2025-06-23 13:44:07 +0300

    6. сделана страница

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Imexam
 
 # Create your views here.
+
+def imexam_list(request):
+    exams = Imexam.objects.get_queryset()
+    return render(request, 'exam/imexam_list.html', {
+        'exams': exams,
+    })
--- myexam/urls.py
+++ myexam/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from exam.views import imexam_list
+from myexam import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('imexam/', imexam_list, name='imexam_list'),
 ]
+
+if settings.DEBUG:
+       urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
