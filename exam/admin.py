3706c7859d815fba9b6d34215258fc4c2a7e9e4b
fultlin <ugolok.kita@gmail.com>
2025-06-23 13:19:19 +0300

    4. Настроена админка для модели

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Imexam
 
-# Register your models here.
+@admin.register(Imexam)
+class ImexamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'exam_date', 'is_public', 'created_at')
+    search_fields = ('name', 'users__email')
+    list_filter = ('is_public', 'created_at', 'exam_date')
+    filter_horizontal = ('users',)
+    date_hierarchy = 'exam_date'
